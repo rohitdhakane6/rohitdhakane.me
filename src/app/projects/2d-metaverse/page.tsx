954ec080264e3a0fe500baba4be3489202fa3ff1
Rohit D. <rohitdhakane6@gmail.com>
2025-12-13 11:54:04 +0530

    chore: remove obsolete project pages for 2D Metaverse and Future Blink

--- src/app/projects/2d-metaverse/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function page() {
-  redirect("https://github.com/rohitdhakane6/metaverse-2d");
-  return <div />;
-}
--- src/app/projects/futureBlink/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function page() {
-  redirect("https://github.com/rohitdhakane6/futureblink-mern-task");
-  return <div />;
-}
